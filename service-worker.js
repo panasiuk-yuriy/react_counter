5e93da18fe228e6dba39387bdcfd39eed3b2d17f
Yuriy Panasiuk <yur.panasiuk@gmail.com>
2020-04-27 14:53:16 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.35369277eabbfb9f41d8e1c2d600fc7c.js"
+  "./precache-manifest.61b5e37a4e420b52692cf44cd43a0259.js"
 );
 
 self.addEventListener('message', (event) => {
